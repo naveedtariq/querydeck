1ffa382e9adc6555c6085faf312a7307cf5e49b4
Umair Siddique <umairsiddique@gmail.com>
2011-12-21 11:37:09 +0500

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Querydeck::Application.config.secret_token = 'c3582e6cbeba45e1660c97c2b1ae7cbab3ade7f8892b3e7b0981c1b98f669f65b9326ffa076357840097f2dc9ae61d23000e9f25959a610f2a6f881a95df986a'
